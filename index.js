9c461d581d45c3b12436eba9dfc86907652f60e3
James <jmosul@users.noreply.github.com>
2020-07-04 16:23:25 +0100

    feat: eslint config

---
+++ index.js
@@ -0,0 +1,11 @@
+module.exports = {
+    rules: {
+        'semi': [2, 'always'],
+        'object-curly-spacing': ['error', 'never'],
+        'indent': ['error', 4, {SwitchCase: 1}],
+        'comma-dangle': ['error', 'always-multiline'],
+        'space-before-function-paren': ['error', 'never'],
+        'no-return-assign': 'off',
+        'no-throw-literal': 'off',
+    },
+};
