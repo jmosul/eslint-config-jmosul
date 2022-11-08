9befa8adb7c0482d100341628eadaaabe1f03c4f
James <jmosul@users.noreply.github.com>
2022-11-08 15:11:32 +0000

    fix: typescript

--- index.js
+++ index.js
@@ -7,5 +7,12 @@ module.exports = {
         'space-before-function-paren': ['error', 'never'],
         'no-return-assign': 'off',
         'no-throw-literal': 'off',
+        '@typescript-eslint/semi': [2, 'always'],
+        '@typescript-eslint/object-curly-spacing': ['error', 'never'],
+        '@typescript-eslint/indent': ['error', 4, {SwitchCase: 1}],
+        'c@typescript-eslint/omma-dangle': ['error', 'always-multiline'],
+        '@typescript-eslint/space-before-function-paren': ['error', 'never'],
+        '@typescript-eslint/no-return-assign': 'off',
+        '@typescript-eslint/no-throw-literal': 'off',
     },
 };
